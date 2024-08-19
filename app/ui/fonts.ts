6ca34a0cad63ad99c04bdee9a444158263cd2897
Kaway Marinho <57325050+kawaym@users.noreply.github.com>
2024-08-19 12:23:22 -0300

    feat(ui/logo.tsx): creates the logo for the application

---
+++ app/ui/fonts.ts
@@ -0,0 +1,8 @@
+import { Raleway } from "next/font/google";
+import { Saira_Stencil_One } from "next/font/google";
+
+export const raleway = Raleway({ weight: ["400", "700"], subsets: ["latin"] });
+export const saira_stencil_one = Saira_Stencil_One({
+  weight: ["400"],
+  subsets: ["latin"],
+});
---
+++ app/ui/logo.tsx
@@ -0,0 +1,11 @@
+import { saira_stencil_one } from "./fonts";
+
+export default function Logo() {
+  return (
+    <p
+      className={`${saira_stencil_one.className} font-normal text-2xl md:text-6xl`}
+    >
+      MyWallet
+    </p>
+  );
+}
