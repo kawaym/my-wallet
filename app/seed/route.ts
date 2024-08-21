315d0466e39081d515857b0df55de8e47e00b5d5
Kaway Marinho <57325050+kawaym@users.noreply.github.com>
2024-08-21 13:11:32 -0300

    feat(seed/route.ts): seeds user

--- app/seed/route.ts
+++ app/seed/route.ts
@@ -5,26 +5,42 @@ import bcrypt from "bcrypt";
 const client = await pool.connect();
 
 async function seedUser() {
-  await client.query(`CREATE EXTESION IF NOT EXISTS "uuid-ossp`);
+  await client.query(`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`);
   await client.query(`
     CREATE TABLE IF NOT EXISTS users (
         id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
         name VARCHAR(255) NOT NULL,
         email TEXT NOT NULL UNIQUE,
-        password TEXT NOT NULL,
-    )  
+        password TEXT NOT NULL
+    );  
   `);
 
   const insertedUsers = await Promise.all(
     users.map(async (user) => {
       const hashedPassword = await bcrypt.hash(user.password, 10);
-      return client.query(`
+      const query = `
             INSERT INTO users (id, name, email, password)
-            VALUES (${user.id}, ${user.name}, ${user.email}, ${hashedPassword})
+            VALUES ('${user.id}', '${user.name}', '${user.email}', '${hashedPassword}')
             ON CONFLICT (id) DO NOTHING;      
-        `);
+        `;
+      console.log(query);
+      return client.query(query);
     })
   );
 
   return insertedUsers;
+}
+
+export async function GET() {
+  try {
+    await client.query("BEGIN");
+    await seedUser();
+    await client.query("COMMIT");
+
+    return Response.json({ message: "Database seeded succesfully" });
+  } catch (error) {
+    await client.query("ROLLBACK");
+    console.log(error);
+    return Response.json({ error }, { status: 500 });
+  }
 }
